5c27a2e4fdbdce97e31706d62fbf574b62f8692d
Kirill Vinogradov <vikian050194@gmail.com>
2023-01-22 17:56:01 +0100

    add two new counters

--- src/common/constants/counters.js
+++ src/common/constants/counters.js
@@ -2,3 +2,5 @@ export const SINCE = "count-since";
 export const OPEN_TOTAL = "count-opening-total";
 export const OPEN_UPDATE = "count-opening-update";
 export const OPEN_CREATE = "count-opening-create";
+export const OPEN_CREATE_KEEP_GROUP = "count-opening-update-keep-group";
+export const OPEN_CREATE_LEAVE_GROUP = "count-opening-create-leave-group";
--- src/counters/counters.js
+++ src/counters/counters.js
@@ -19,6 +19,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     const total = update + create;
     $total.innerText = total;
 
+    const $keepGroup = document.getElementById(COUNTERS.OPEN_CREATE_KEEP_GROUP);
+    const keep = await Local.get(COUNTERS.OPEN_CREATE_KEEP_GROUP);
+    $keepGroup.innerText = keep;
+
+    const $leave = document.getElementById(COUNTERS.OPEN_CREATE_LEAVE_GROUP);
+    const leave = await Local.get(COUNTERS.OPEN_CREATE_LEAVE_GROUP);
+    $leave.innerText = leave;
+
     const $updatePercent = document.getElementById(`${COUNTERS.OPEN_UPDATE}-percent`);
     const updatePercent = total > 0 ? (100 * update / total).toFixed(2) : "0.00";
     $updatePercent.innerText = ` (${updatePercent}%)`;
@@ -27,11 +35,23 @@ document.addEventListener("DOMContentLoaded", async () => {
     const createPercent = total > 0 ? (100.00 - parseFloat(updatePercent)).toFixed(2) : "0.00";
     $createPercent.innerText = ` (${createPercent}%)`;
 
+    const $keepPercent = document.getElementById(`${COUNTERS.OPEN_CREATE_KEEP_GROUP}-percent`);
+    const keepPercent = create > 0 ? (100 * keep / create).toFixed(2) : "0.00";
+    $keepPercent.innerText = ` (${keepPercent}%)`;
+
+    const $leavePercent = document.getElementById(`${COUNTERS.OPEN_CREATE}-percent`);
+    const leavePercent = create > 0 ? (100 * leave / create).toFixed(2) : "0.00";
+    $leavePercent.innerText = ` (${leavePercent}%)`;
+
     const $resetButton = document.getElementById("reset");
     $resetButton.addEventListener("click", async () => {
         await Local.set(COUNTERS.SINCE, new Date().toISOString());
 
         await Local.set(COUNTERS.OPEN_UPDATE, 0);
         await Local.set(COUNTERS.OPEN_CREATE, 0);
+
+        await Local.set(COUNTERS.OPEN_CREATE_KEEP_GROUP, 0);
+        await Local.set(COUNTERS.OPEN_CREATE_LEAVE_GROUP, 0);
+
     });
 });
