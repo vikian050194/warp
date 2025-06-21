817f61fc968c9c9ed40cb0b50627d6471ba8d16d
Kirill Vinogradov <vikian050194@gmail.com>
2025-06-21 13:58:29 +0200

    add unit test

--- tests/filters/complex/mix-tests.js
+++ tests/filters/complex/mix-tests.js
@@ -75,4 +75,21 @@ describe("complex mix filter", function () {
 
         assert.deepEqual(actual, expected);
     });
+
+    // TODO add more unit tests for ComplexFilter
+    it("title before abbreviation", function () {
+        const query = "aa";
+        const bookmarks = [
+            new BookmarkModel("1", "url1", "ac", ["ab"]),
+            new BookmarkModel("2", "url2", "aaf", ["dir1"])
+        ];
+        const expected = [
+            new BookmarkModel("2", "url2", "aaf", ["dir1"]),
+            new BookmarkModel("1", "url1", "ac", ["ab"])
+        ];
+
+        const actual = filter(query, bookmarks);
+
+        assert.deepEqual(actual, expected);
+    });
 });
